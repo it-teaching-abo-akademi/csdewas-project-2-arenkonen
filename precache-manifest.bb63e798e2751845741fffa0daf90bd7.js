5bbefa32797856fb0ae8809d597b0bb9d4f027e0
arenkonen <alex.renkonen@gmail.com>
2020-01-16 17:16:26 +0200

    Updates

---
+++ precache-manifest.bb63e798e2751845741fffa0daf90bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc746b07b6bd98aaa19c2f62df3db5b",
+    "url": "/csdewas-project-2-arenkonen/index.html"
+  },
+  {
+    "revision": "f9441621838821aec302",
+    "url": "/csdewas-project-2-arenkonen/static/css/main.fe82fa91.chunk.css"
+  },
+  {
+    "revision": "41489bea063d9cccee20",
+    "url": "/csdewas-project-2-arenkonen/static/js/2.4089c330.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/csdewas-project-2-arenkonen/static/js/2.4089c330.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9441621838821aec302",
+    "url": "/csdewas-project-2-arenkonen/static/js/main.eb7cbf8e.chunk.js"
+  },
+  {
+    "revision": "25f239de35f34d3e0333",
+    "url": "/csdewas-project-2-arenkonen/static/js/runtime-main.8316852a.js"
+  }
+]);
